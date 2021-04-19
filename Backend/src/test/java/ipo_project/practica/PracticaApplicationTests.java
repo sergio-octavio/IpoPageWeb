66b0fbfcfdd94a5aef87ad8568981974e10f806c
expiQ479 <61882281+expiQ479@users.noreply.github.com>
2021-04-19 19:27:36 +0200

    maven

---
+++ Backend/src/test/java/ipo_project/practica/PracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package ipo_project.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
