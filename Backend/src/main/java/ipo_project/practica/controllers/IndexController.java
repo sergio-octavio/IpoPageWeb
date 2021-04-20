3d997fb211cf0c41ebe08d3380af94e709174643
expiQ479 <61882281+expiQ479@users.noreply.github.com>
2021-04-20 14:12:32 +0200

    updating maven

--- Backend/src/main/java/ipo_project/practica/controllers/IndexController.java
+++ Backend/src/main/java/ipo_project/practica/controllers/IndexController.java
@@ -1,13 +1,14 @@
 package ipo_project.practica.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String showindex() {  
+    public String showindex(Model model) {  
         return "index";
     }   
 }
